ead4e8daaa3af5809f16548809fb08781b6bc953
imriSh <59433290+imriShCodeArt@users.noreply.github.com>
2022-09-11 08:07:44 +0300

    different approach all-over
    completely replace all components with updated ones. importing whole library from the beginning.

--- src/App.js
+++ src/App.js
@@ -1,76 +1,20 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { hot } from "react-hot-loader/root";
 
-import './index.css'
-import { BrowserRouter, useNavigate } from 'react-router-dom'
-import Menu from '../shared/Menu'
-import { Dashboard, Info, Logout, Settings } from '@mui/icons-material/'
-import Avatar from '../shared/Avatar'
-import { Stack } from '@mui/material'
-
-// const MyTheme = React.lazy(() => import('theme/Theme'))
-// const Theme = ({ children, ...rest }) => (
-//   <React.Suspense fallback={<div />}>
-//     <MyTheme {...rest} >
-//       {children}
-//     </MyTheme>
-//   </React.Suspense>
-// )
+//  ******* clears console after each code update ******
+window.addEventListener("message", function onWebpackMessage(e) {
+  console.clear();
+});
+//  *****************************************************
 
 const App = () => {
-  const navigation = useNavigate()
-  const items = [
-    {
-      text: 'Profile',
-      action: () => navigation('settings/profile'),
-      icon: <Dashboard />,
-    },
-    {
-      text: 'Account',
-      action: () => navigation('settings/Account'),
-      icon: <Info />,
-    },
-    {
-      text: 'Settings',
-      action: () => navigation('settings'),
-      icon: <Settings />,
-    },
-    {
-      text: 'divider',
-    },
-    {
-      text: 'Log Out',
-      action: () => navigation('logout'),
-      icon: <Logout />,
-    },
-  ]
+
   return (
     <>
-      <Menu anchorText={'Click'} items={items} />
-      {['xs', 'sm', 'md', 'lg', 'xl', 'xxl'].map((i, index) => (
-        <Stack key={index} direction={'row'}>
-          <Avatar
-            size={i}
-            imgSrc={
-              'https://miro.medium.com/fit/c/176/176/1*bj4j4IqF5ov3jeha_2tRkQ.jpeg'
-            }
-            items={items}
-          />
-          <Avatar
-            key={index}
-            size={i}
-            text={'T'}
-            items={items}
-          />
-        </Stack>
-      ))}
+    APP
     </>
   )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('app'))
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+export default hot(App);
+
