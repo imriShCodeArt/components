73dfe9bc335ff7b7760f20db0430ea778e1ef25d
imriSh <59433290+imriShCodeArt@users.noreply.github.com>
2022-09-13 11:25:18 +0300

    Create Button

---
+++ shared/io/Button/index.js
@@ -0,0 +1,78 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import Root from "@mui/material/Button";
+
+function Button({
+  color,
+  variant,
+  onClick,
+  id,
+  controls,
+  text,
+  children,
+  open,
+  contained,
+  outlined,
+  secondary,
+  primary,
+  title,
+  inherit,
+  info,
+  warning,
+  success,
+  ...rest
+}) {
+  return (
+    <Root
+      aria-controls={controls}
+      aria-haspopup={controls ? "true" : undefined}
+      aria-expanded={open ? "true" : undefined}
+      variant={contained ? "contained" : outlined ? "outlined" : variant}
+      color={
+        primary
+          ? "primary"
+          : secondary
+          ? "secondary"
+          : inherit
+          ? "inherit"
+          : info
+          ? "info"
+          : warning
+          ? "warning"
+          : success
+          ? "success"
+          : color
+      }
+      {...{ onClick, id, title, ...rest }}
+    >
+      {children || text}
+    </Root>
+  );
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  children: PropTypes.string,
+  variant: PropTypes.oneOf(["text", "contained", "outlined"]),
+  onClick: PropTypes.func,
+  id: PropTypes.string,
+  controls: PropTypes.string, // the id of the element which is controlled by this button
+  open: PropTypes.bool,
+  color: PropTypes.oneOf([
+    "inherit",
+    "primary",
+    "secondary",
+    "info",
+    "warning",
+    "success",
+    "default",
+  ]),
+  title: PropTypes.string,
+};
+
+Button.defaultProps = {
+  onClick: () => ({}),
+};
+
+export default Button;
